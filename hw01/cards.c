b54da0cc514967d34ee504794bad5b7f68185cd2
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2014-01-28 19:47:23 -0500

    Added docstrings.

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+Prompts the user for a playing card name, then counts cards based
+on user input.
+Authors: Samantha Kumarasena, Head First C
+*/
 int main() {
 	char card_name[3];
 	int count = 0;
@@ -31,6 +35,10 @@ int main() {
 	return 0;
 }
 
+/*
+Prints an error when the card value is invalid.
+val: value of card provided by user
+*/
 int print_error(int val) {
 	if((val<1) || (val>10)) {
 	puts("I don't understand that value!");
@@ -38,6 +46,10 @@ int print_error(int val) {
 	return 0;
 }
 
+/*
+Changes the card count depending on the value of the card.
+val: value of card provided by user
+*/
 int change_count(int val) {
 	int c = 0;
 	if ((val>2) && (val<7)) {
