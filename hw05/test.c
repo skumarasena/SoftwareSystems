aa7d500639f790ef6e815b9cd95edaf1380b2dc1
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2014-04-01 16:29:42 -0400

    Adding function descriptions.

--- hw05/test.c
+++ hw05/test.c
@@ -8,18 +8,26 @@
 #include <stdlib.h>
 
 
+/*Prints a single key-value pair in the hash table. For use with print_hash_table.*/
 
 static void print_row(gpointer key, gpointer val, gpointer userdata) {
     GString* realKey = (GString*)key;
-    printf("key: %s; val: %i\n", realKey->str, *(int*)val);
+    printf("Key: %s, Value: %i\n", realKey->str, *(int*)val);
 
 }
+
+
+
+/*Prints a hash table.*/ 
 
 void print_hash_table(GHashTable* hash) {
     g_hash_table_foreach(hash, print_row, NULL);
 }
 
 
+
+/*Adds a word to a hash table. If it is not yet present in the hash table, it is added.
+If it is present, the value (word count) is incremented.*/
 
 void hash_append(GHashTable* hash, GString* word)
 {
@@ -37,16 +45,20 @@ void hash_append(GHashTable* hash, GString* word)
 	
 }
 
+/*Reads a file and adds key-value pairs to a hash table of the form (word, word count).*/
+
 void fill_table(GHashTable* hash, FILE* file) {
     char* temp = malloc(sizeof(char)*100);
     while(1) {
-    if(fscanf(file, "%s", temp)!=1) {
-        break;
+        if(fscanf(file, "%s", temp)!=1) {
+            break;
+        }
+        hash_append(hash, g_string_new(temp));
     }
-    hash_append(hash, g_string_new(temp));
-}
 
 }
+
+/*Prompts a user for a file name, and prepares the file to be read.*/
 
 FILE* make_file() {
     char* filename = malloc(sizeof(char)*80);
